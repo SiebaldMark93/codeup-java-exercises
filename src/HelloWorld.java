b544e7bdd9b3dcdbc94c96215a40c7ae6cc7af11
Mark Siebald <marksiebald@Marks-Air.attlocal.net>
2022-06-06 22:26:02 -0500

    finished syntax types and variables exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,26 +2,59 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 //        1.
-        int myFavoriteNumber = 8;
-        System.out.println(myFavoriteNumber);
+//        int myFavoriteNumber = 8;
+//        System.out.println(myFavoriteNumber);
 //        2.
-        String myString = "I am so lost.";
-        System.out.println(myString);
+//        String myString = "";
+//        System.out.println(myString);
 //        3.
+//        String myString = "I am so lost!";
+//        System.out.println(myString);
+//        4.
+//        String myString = 3.14159;
+//        System.out.println(myString);
+//        5.
+//        long myNumber;
+//        System.out.println(myNumber);
+//        6.
+//        long myNumber = 3.14;
+//        System.out.println(myNumber);
+//        7.
+//        long myNumber = 123L;
+//        System.out.println(myNumber);
+//        8.
+//        long myNumber = 123;
+//        System.out.println(myNumber);
 //        9.
-        float myNumber = 3.14F;
-        System.out.println(myNumber);
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+//        10.
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        11.
+//        Variable does not load or exist.
+//        12.
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//
+//        int three = (int) "three";
 //        13.
 //        int x = 4;
 //        x = x + 5;
 //        x += 5;
 //        System.out.println(x);
-        int x = 10;
-        int y = 2;
+//        int x = 10;
+//        int y = 2;
 //        x = x /y;
-        x /= y;
-        y -= x;
-        System.out.println(x);
-        System.out.println(y);
+//        x /= y;
+//        y -= x;
+//        System.out.println(x);
+//        System.out.println(y);
+
     }
 }
