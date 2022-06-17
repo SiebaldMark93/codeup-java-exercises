ab2ee07082fdf40a8f5a112ff2f4d6e503b97d6d
Mark Siebald <Siebald.Mark93@gmail.com>
2022-06-17 14:02:04 -0500

    feat: working on student class

---
+++ src/Grades/Student.java
@@ -0,0 +1,16 @@
+package Grades;
+
+
+import java.util.ArrayList;
+
+public class Student {
+    public static void main(String[] args) {
+        private String
+        // returns the student's name
+        public String getName();
+// adds the given grade to the grades property
+        public void addGrade(int grade);
+// returns the average of the students grades
+        public double getGradeAverage();
+    }
+}
